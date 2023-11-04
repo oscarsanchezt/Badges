8b2a97c2b49bf9f153e4dd44f40734be26b0f458
Piramidex <49204633+oscarsanchezt@users.noreply.github.com>
2023-11-04 01:02:28 +0100

    Create Code64.py

---
+++ Code64.py
@@ -0,0 +1,11 @@
+import base64
+
+texto_original = "Code."
+
+texto_codificado = base64.b64encode(texto_original.encode()).decode()
+
+print("Texto codificado en base64:", texto_codificado)
+
+texto_decodificado = base64.b64decode(texto_codificado).decode()
+
+print("Texto decodificado:", texto_decodificado)
